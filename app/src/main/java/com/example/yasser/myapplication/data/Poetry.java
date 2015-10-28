c7820b4584cb36db84379efd8613d29f2cc7094d
othman853 <anuar.yasser@gmail.com>
2015-10-28 20:57:30 -0200

    Some poetries listing

---
+++ app/src/main/java/com/example/yasser/myapplication/data/Poetry.java
@@ -0,0 +1,41 @@
+package com.example.yasser.myapplication.data;
+
+/**
+ * Created by yasser on 10/28/15.
+ */
+public class Poetry {
+
+    private String title;
+    private String content;
+
+    public Poetry() {
+    }
+
+    public Poetry(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Poetry deserialize(String serial) {
+        String [] exploded = serial.split("[|]");
+        title = exploded[0];
+        content = exploded[1];
+        return this;
+    }
+}
--- app/src/main/java/com/example/yasser/myapplication/sections/PoetryFragment.java
+++ app/src/main/java/com/example/yasser/myapplication/sections/PoetryFragment.java
@@ -3,22 +3,36 @@ package com.example.yasser.myapplication.sections;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.yasser.myapplication.R;
+import com.example.yasser.myapplication.adapters.PoetryListAdapter;
 
 /**
  * Created by yasser on 10/27/15.
  */
-public class PoetryFragment extends Fragment{
+public class PoetryFragment extends Fragment implements AdapterView.OnItemClickListener{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.poetry_fragment, container,false);
 
+        ListView poetryListView = (ListView) view.findViewById(R.id.poetry_list_view);
+
+        poetryListView.setAdapter(new PoetryListAdapter(getActivity()));
+        poetryListView.setOnItemClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("Sup", "Thats a click");
     }
 }
