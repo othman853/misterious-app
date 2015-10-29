8dfa3ca8929ab69b63e901a0a7b9e01f5793919d
othman853 <anuar.yasser@gmail.com>
2015-10-29 05:10:18 -0200

    Opening internals

--- app/src/main/java/com/example/yasser/myapplication/sections/PoetryFragment.java
+++ app/src/main/java/com/example/yasser/myapplication/sections/PoetryFragment.java
@@ -1,6 +1,7 @@
 package com.example.yasser.myapplication.sections;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -12,6 +13,8 @@ import android.widget.ListView;
 
 import com.example.yasser.myapplication.R;
 import com.example.yasser.myapplication.adapters.PoetryListAdapter;
+import com.example.yasser.myapplication.sections.internals.PoetryInternalFragment;
+import com.example.yasser.myapplication.util.Constants;
 
 /**
  * Created by yasser on 10/27/15.
@@ -33,6 +36,14 @@ public class PoetryFragment extends Fragment implements AdapterView.OnItemClickL
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        transaction
+                .replace(R.id.main_content, new PoetryInternalFragment())
+                .addToBackStack(Constants.FragmentTags.POETRY)
+                .commit();
         Log.i("Sup", "Thats a click");
     }
 }
